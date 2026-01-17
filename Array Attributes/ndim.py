7cb0bd098faaa548f6024feaffe5682691eaf880
bhavyaa-1001 <bhavyyaa106@gmail.com>
2026-01-18 02:50:55 +0530

    ndim

---
+++ Array Attributes/ndim.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a1 = np.arange(10,dtype=np.int32)
+a2 = np.arange(12,dtype=float).reshape(3,4)
+a3 = np.arange(8).reshape(2,2,2)
+
+print("Array a1:\n", a1)
+print("Array a2:\n", a2)
+print("Array a3:\n", a3)
+print("Number of dimensions of a1:", a1.ndim)
+print("Number of dimensions of a2:", a2.ndim)
+print("Number of dimensions of a3:", a3.ndim)
